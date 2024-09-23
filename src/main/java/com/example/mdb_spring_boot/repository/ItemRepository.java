83792485952582f2f10def9ee399154e5ae7e23b
Dongwook Kim <dongwook227@gmail.com>
2024-09-23 07:47:14 -0400

    add interface

---
+++ src/main/java/com/example/mdb_spring_boot/repository/ItemRepository.java
@@ -0,0 +1,17 @@
+package com.example.mdb_spring_boot.repository;
+
+import com.example.mdb_spring_boot.model.GroceryItem;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface ItemRepository extends MongoRepository<GroceryItem, String> {
+    @Query("{name: '?0'}")
+    GroceryItem findItemByName(String name);
+
+    @Query(value="{category: '?0'}", fields="{'name':  1, 'quantity':  1}")
+    List<GroceryItem> findAll(String category);
+
+    public long count();
+}
