7bdf7dfba85ea1d27f2add97a899b4633251ae02
Dongwook Kim <dongwook227@gmail.com>
2024-09-23 07:19:45 -0400

    first commit

---
+++ src/test/java/com/example/mdb_spring_boot/MdbSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mdb_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdbSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
