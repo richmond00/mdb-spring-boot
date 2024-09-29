effa8dadae4ca9e9d90e6baa68cd22a01028d5a5
Dongwook Kim <dongwook227@gmail.com>
2024-09-29 17:56:53 -0400

    add getters and setters

--- src/main/java/com/example/mdb_spring_boot/model/GroceryItem.java
+++ src/main/java/com/example/mdb_spring_boot/model/GroceryItem.java
@@ -21,5 +21,35 @@ public class GroceryItem {
         this.category = category;
     }
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getItemQuantity() {
+        return quantity;
+    }
+
+    public void setItemQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
