d2941ce5c3a7b0b620d4e30974f7748db8416afb
Dongwook Kim <dongwook227@gmail.com>
2024-10-02 07:53:43 -0400

    create repository interface

---
+++ src/main/java/com/example/mdb_spring_boot/repository/TutorialRepository.java
@@ -0,0 +1,11 @@
+package com.example.mdb_spring_boot.repository;
+
+import com.example.mdb_spring_boot.model.Tutorial;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends MongoRepository<Tutorial, String> {
+    List<Tutorial> findByTitleContaining(String title);
+    List<Tutorial> findByPublished(boolean published);
+}
