2483f8717e36c847872fdd94244b9857ddcfd143
Dongwook Kim <dongwook227@gmail.com>
2024-10-03 10:20:06 -0400

    add getter/setter

--- src/main/java/com/example/mdb_spring_boot/model/Tutorial.java
+++ src/main/java/com/example/mdb_spring_boot/model/Tutorial.java
@@ -25,4 +25,25 @@ public class Tutorial {
     public String getId() {
         return id;
     }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public boolean isPublished() {
+        return published;
+    }
+    public void setPublished(boolean isPublished) {
+        this.published = isPublished;
+    }
+    public String toString() {
+        return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published" + published + "]";
+    }
 }
