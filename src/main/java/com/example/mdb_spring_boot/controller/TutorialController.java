e1db5a415f17764fef611f9eae6a92aab88ff8ea
Dongwook Kim <dongwook227@gmail.com>
2024-10-03 10:28:44 -0400

    add create

--- src/main/java/com/example/mdb_spring_boot/controller/TutorialController.java
+++ src/main/java/com/example/mdb_spring_boot/controller/TutorialController.java
@@ -27,7 +27,12 @@ public class TutorialController {
 
     @PostMapping("/tutorials")
     public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
-        return null;
+        try {
+            Tutorial _tutorial = tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
+            return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
